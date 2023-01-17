445259f2427c70139ad3c591f53a3104a9034854
Ahmad khaled Elshafee <60294043+elshafee@users.noreply.github.com>
2023-01-17 19:54:07 +0200

    updating to last version
    authentication with firebase

---
+++ AndroidClasswin 7.53.23 PM/app/src/main/java/com/elshafee/androidclasswin/shoppingitemlist/db/ShoppingDao.kt
@@ -0,0 +1,27 @@
+package com.elshafee.androidclasswin.shoppingitemlist.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.elshafee.androidclasswin.shoppingitemlist.model.ShoppingItem
+
+
+@Dao
+interface ShoppingDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(item:ShoppingItem)
+
+
+    @Delete
+    fun delete(item: ShoppingItem)
+
+    @Query("SELECT * FROM shopping_items")
+    fun getAllShoppingItem(): LiveData<List<ShoppingItem>>
+
+
+
+}
